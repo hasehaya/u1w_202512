3d01c31ed094d8988899438a9ce394e65812b6df
jumpsea10 <h2110529@gl.cc.uec.ac.jp>
2025-12-28 15:03:34 +0900

    add exit process into Loading

--- Assets/Scripts/PhaseController/LoadingPhaseController.cs
+++ Assets/Scripts/PhaseController/LoadingPhaseController.cs
@@ -10,7 +10,9 @@ public class LoadingPhaseController : PhaseController
     [SerializeField] private Sprite[] images;
     [SerializeField] private Image loadingImage;
     [SerializeField] private float interval = 0.5f; // 画像切り替え間隔（秒）
-
+    [SerializeField] private float loadingDuration = 3.0f; // ローディング画面の表示時間（秒）
+    
+    private float passedTime;
     private float elapsedTime;
     private int currentImageIndex;
 
@@ -21,11 +23,18 @@ public class LoadingPhaseController : PhaseController
         loadingImage.sprite = images[0];
         elapsedTime = 0f;
         currentImageIndex = 0;
+        passedTime = 0f;
     }
 
     public override void UpdatePhase()
     {
         elapsedTime += Time.deltaTime;
+        passedTime += Time.deltaTime;
+        if (passedTime >= loadingDuration)
+        {
+            CompleteLoading();
+            return;
+        }
         if (elapsedTime >= interval)
         {
             elapsedTime = 0f;
@@ -33,10 +42,10 @@ public class LoadingPhaseController : PhaseController
             loadingImage.sprite = images[currentImageIndex];
         }
     }
-
+    
     protected override void OnExitImpl()
     {
-        
+
     }
 
     private void CompleteLoading()
